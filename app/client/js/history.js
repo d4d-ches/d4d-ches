0569526c07e5679da42d7fc77f9878eba2c5ae56
anmolgupta23 <anmolgupta@college.harvard.edu>
2015-03-29 22:00:15 -0400

    history

--- app/client/js/history.js
+++ app/client/js/history.js
@@ -1,5 +1,29 @@
 Template.page_history.helpers({
     all: function(){
         return History.find({});
+    },
+    
+    filtered: function(){
+    	var date = Session.get('date');
+    	if(!date){
+    		return History.find({});
+    	}
+    	else{
+    		return History.find({
+    			"sent": date
+    		});
+    	}
     }
+});
+
+Template.page_history.events({
+	'submit #history-filter': function(event){
+		// figure out what date they chose
+		var form = $(event.target);
+		var month = form.find('#history-month').val();
+		var day = form.find('#history-day').val();
+		var year = form.find('#history-year').val();
+		var date = month + " " + day + ", " + year;
+		Session.set('date', date);
+	}
 });

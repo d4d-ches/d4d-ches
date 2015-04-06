4ddf05e9edf1e88c2bb602488e26a958c1331385
scheng95 <samuelcheng@college.harvard.edu>
2015-04-05 22:19:45 -0400

    sorted conversations

--- app/client/js/conversations.js
+++ app/client/js/conversations.js
@@ -29,6 +29,21 @@ Template.page_conversations.helpers({
             return null;
         }
     },
+    messagesConversation: function(){
+        var entrepreneur = Session.get('chosenEntrepreneur');
+        var to_chosen = messagesToChosen();
+        var from_chosen = messagesFromChosen();
+        var conversation = _.union(to_chosen, from_chosen);
+        var sorted = _.sortBy(conversation, function(item){
+            return -1 * moment(new Date(item.created_at)).unix();
+        });
+        _.each(sorted, function(message){
+            if (message.sender_screen_name === entrepreneur.twitter){
+                message.received = true;
+            };
+        });
+        return sorted;
+    },
 
     all_received_messages: function(){
         return Received.find({});
@@ -44,3 +59,29 @@ Template.page_conversations.events({
         Session.set('chosenEntrepreneur', entrepreneur);
     }
 });
+
+function messagesFromChosen(){
+    var entrepreneur = Session.get('chosenEntrepreneur');
+    if(entrepreneur){
+        var twitter = entrepreneur.twitter;
+        return Received.find({
+            sender_screen_name: twitter
+        }).fetch();
+    }
+    else {
+        return null;
+    }
+}
+
+function messagesToChosen(){
+    var entrepreneur = Session.get('chosenEntrepreneur');
+    if(entrepreneur){
+        var twitter = entrepreneur.twitter;
+        return History.find({
+            recipient_screen_name: twitter
+        }).fetch();
+    }
+    else {
+        return null;
+    }
+}

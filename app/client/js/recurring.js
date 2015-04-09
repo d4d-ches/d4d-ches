ba9b83e4d14e16d441f0ff528b5094a902ad471f
hathix <neelmehta@college.harvard.edu>
2015-04-08 21:46:48 -0400

    added to recurring code

--- app/client/js/recurring.js
+++ app/client/js/recurring.js
@@ -9,6 +9,16 @@ Template.page_send.events({
     'submit #recurring-new': function(event){
         var form = event.target;
 
+        /*
+            TODO
+            grab question, timing
+            store in recurring
+            also store last_sent
+
+            in init, check if now - last_sent > recurring_period; if so send and reset last_sent
+
+        */
+
         return false;
     }
 });
